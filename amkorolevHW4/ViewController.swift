43a305893c587b3be467f06511f6e6a73091e8a0
korolev solutions <neoevil25@gmail.com>
2022-03-19 02:16:57 +0500

    Initial Commit

---
+++ amkorolevHW4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  amkorolevHW4
+//
+//  Created by Андрей Королев on 19.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
