dc45ade6611acc2eef7a42f8c093d31e918f74ab
korolev solutions <neoevil25@gmail.com>
2022-03-27 21:49:31 +0500

    point 7: delete note functionality added

--- amkorolevHW4/ViewController.swift
+++ amkorolevHW4/ViewController.swift
@@ -87,7 +87,17 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
         return cell
     }
     
-
+    func collectionView(_ collectionView: UICollectionView, contextMenuConfigurationForItemAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
+        let identifier = "\(indexPath.row)" as NSString
+        return UIContextMenuConfiguration(identifier: identifier, previewProvider: .none) { _ in
+            let deleteAction = UIAction(title: "Delete", image: UIImage(systemName: "trash"), attributes: UIMenuElement.Attributes.destructive) { value in
+                self.context.delete(self.notes[indexPath.row])
+                self.saveChanges()
+            }
+            
+            return UIMenu(title: "", image: nil, children: [deleteAction])
+        }
+    }
 }
 
 class NoteCell: UICollectionViewCell {
